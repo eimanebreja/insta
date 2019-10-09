7fa8fcc5c1da49aca143157c9bba4da5a6ea2bff
Nimuel Eiman Nebreja <nimuel24@gmail.com>
2019-10-09 09:07:22 +0800

    Adding Mail

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\User;
 
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -15,8 +16,27 @@ class ProfilesController extends Controller
 
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
 
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->posts->count();
+            });
+        $followersCount = Cache::remember(
+            'count.followers.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->profile->followers->count();
+            });
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->following->count();
+            });
 
-        return view('profiles.index', compact('user', 'follows'));
+
+            return view('profiles.index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount'));
     }
 
 
